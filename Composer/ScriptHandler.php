0294ce77e276ee0d817dc7f5a1e18dcee06d006b
Ariel Ferrandini Price <ariel@ferrandini.com>
2012-11-21 18:05:59 +0100

    Update composer requires, better integration

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -1,18 +1,17 @@
 <?php
 
 /*
+  * @author Ariel Ferrandini <arielferrandini@gmail.com>
  *
- * Ariel Ferrandini
- * arielferrandini@gmail.com
- *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Ferrandini\Composer;
 
-/**
- * @author Ariel Ferrandini
- */
-class ScriptHandler
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as BaseHandler;
+
+class ScriptHandler extends BaseHandler
 {
     public static function installBinUtils($event)
     {
@@ -20,7 +19,7 @@ class ScriptHandler
         $appDir = $options['symfony-app-dir'];
 
         if (!is_dir($appDir)) {
-            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not install bin utils.'.PHP_EOL;
+            $event->getIO()->write('The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not install bin utils.</error>');
 
             return;
         }
@@ -28,44 +27,30 @@ class ScriptHandler
         $binDir = $options['symfony-app-dir'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'bin';
 
         if (!is_dir($binDir)) {
-            echo 'Creating /bin dir.'.PHP_EOL;
+            $event->getIO()->write('Creating /bin dir.');
 
             if (!mkdir($binDir)) {
-                echo 'Cannot create /bin dir.'.PHP_EOL;
+                $event->getIO()->write('Cannot create /bin dir.');
 
                 return;
             }
         }
 
         if (!is_writable($binDir)) {
-            echo '/bin dir is not writable.'.PHP_EOL;
+            $event->getIO()->write('<error>/bin dir is not writable.</error>');
         }
 
-        echo "Installing bin/fixperms script.".PHP_EOL;
+        $event->getIO()->write('<info>Installing bin/fixperms script.</info>');
         $fixperms_gist_url = 'https://raw.github.com/gist/4087091/e9827573fb3cc0675d63975ec104e9e63103b2ae/fixperms';
         $fixperms = file_get_contents($fixperms_gist_url);
         file_put_contents($binDir.DIRECTORY_SEPARATOR.'fixperms', $fixperms);
         chmod($binDir.DIRECTORY_SEPARATOR.'fixperms', 0755);
 
-        echo "Installing bin/clearcache script.".PHP_EOL;
+        $event->getIO()->write('<info>Installing bin/clearcache script.</info>');
         $clearcache_gist_url = 'https://raw.github.com/gist/4088354/f34d91df3e81df9eba8e4d94f1c0c80180f514cd/clearcache';
         $clearcache = file_get_contents($clearcache_gist_url);
         file_put_contents($binDir.DIRECTORY_SEPARATOR.'clearcache', $clearcache);
         chmod($binDir.DIRECTORY_SEPARATOR.'clearcache', 0755);
-
     }
 
-    protected static function getOptions($event)
-    {
-        $options = array_merge(array(
-            'symfony-app-dir' => 'app',
-            'symfony-web-dir' => 'web',
-            'symfony-assets-install' => 'hard'
-            ), $event->getComposer()->getPackage()->getExtra());
-
-        $options['symfony-assets-install'] = getenv('SYMFONY_ASSETS_INSTALL') ?: $options['symfony-assets-install'];
-
-        $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
-        return $options;
-    }
 }
